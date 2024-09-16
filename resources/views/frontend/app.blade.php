359c91eb85507959ed7364ee4896cf00cc06ac5f
bisma233 <bismadilawar5@gmail.com>
2024-09-16 11:55:14 +0500

    abc

--- resources/views/frontend/app.blade.php
+++ resources/views/frontend/app.blade.php
@@ -30,9 +30,8 @@
                     <li class="nav-item"><a href="{{ route('blog') }}">Blog</a></li>
                     {{-- <li class="nav-item"><a href="{{ route('projects') }}">Projects</a></li> --}}
                     {{-- <li class="nav-item"><a href="{{ route('netmetering') }}">Net Metering</a></li> --}}
-                    <li class="nav-item"><a href="{{ route('install-a-system') }}">Install a system</a></li>
                     {{-- <li class="nav-item"><a href="{{ route('financing') }}">Financing</a></li> --}}
-                   <li class="nav-item login"><a href="{{ route('registeration') }}">Login</a></li>
+                   <!--<li class="nav-item login"><a href="{{ route('registeration') }}">Login</a></li> -->
                     
                 </ul>
             </div>
